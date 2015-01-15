e6d93a83a451f365966db7ee85fe3e5fcb09ba01
demetron <dmitriy_zh@ukr.net>
2015-01-15 03:00:09 +0200

    Add function 'insertValues'

--- binder.js
+++ binder.js
@@ -9,9 +9,18 @@
         addArrValues: function(){
             
         },
+        
     };
     
-    
+    function insertValues(name, value) {
+        var patternConstructor = "/{{"+name+"}}/";
+        var pattern = new RegExp(patternConstructor, "gi");
+        var body = document.getElementsByTagName("body")[0];
+        //var bodyText = document.getElementsByTagName("body")[0].innerHTML;
+        var bodyText = body.innerHTML;
+        bodyLikeText.replace(pattern, value);
+        body.innerHTML = bodyText;
+    }
     
     
     return B;

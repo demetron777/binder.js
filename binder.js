719145e66b63d094a6fbf0042fc9fada4cfd74c2
demetron <dmitriy_zh@ukr.net>
2015-01-15 12:35:17 +0200

    Add function 'init'

--- binder.js
+++ binder.js
@@ -1,21 +1,25 @@
 (function(w, d){
-    var B = {
-        dataArr:[
-            
-        ],
-        addValue:function(name, data){
-            
+    var $B = {
+        dataArr:{},
+        addValue:function(name, value){
+            this.dataArr[name] = value;
         },
-        addArrValues: function(){
-            
+        addArrValues: function(objVal){
+            for (var name in objVal) {
+                this.addValue(name, objVal[name]);
+            }
         },
-        
+        init: function(){
+            for (var key in this.dataArr) {
+                this.insert(key, this.dataArr[key]);
+            }
+        },
+        insert: insertValues()
     };
     
     function insertValues(name, value) {
         var patternConstructor = "/{{"+name+"}}/";
-        var pattern = new RegExp(patternConstructor, "gi");//test
-
+        var pattern = new RegExp(patternConstructor, "gi");
         var body = d.getElementsByTagName("body")[0];
         //var bodyText = document.getElementsByTagName("body")[0].innerHTML;
         var bodyText = body.innerHTML;
@@ -23,6 +27,5 @@
         body.innerHTML = bodyText;
     }
     
-    
-    return B;
+    return $B;
 })(window, document);

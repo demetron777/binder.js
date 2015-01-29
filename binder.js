d61de5edc51b83eacb1b73da81c4a5b946f361fd
demetron <dmitriy_zh@ukr.net>
2015-01-29 11:35:48 +0200

    add universal databinding function

--- binder.js
+++ binder.js
@@ -11,17 +11,21 @@ function addEvent(evnt, elem, func) {
 addEvent("DOMContentLoaded", document, function(event) {
     (function(){
         var binder = {
-            dataArrGlob:{},
-            dataArrLoc:{},
+            data:{
+               byDocument:{},
+               byBody:{},
+               byId:{},
+               byClass:{},
+            }
             addValue:function(n, v, id){
                 if (arguments.length == 3) {
-                    if(this.dataArrLoc[id] == undefined){
-                        this.dataArrLoc[id] = {};
+                    if(this.data.byId[id] == undefined){
+                        this.data.byId[id] = {};
                     }
-                    this.dataArrLoc[id][n] = v;
+                    this.data.byId[id][n] = v;
                 }
                 else if(arguments.length == 2) {
-                    this.dataArrGlob[n] = v;
+                    this.data.byBody[n] = v;
                 }
             },
             addArrValues: function(objVal){
@@ -30,24 +34,24 @@ addEvent("DOMContentLoaded", document, function(event) {
                 }
             },
             initData: function(){
-                for (var keyGlob in this.dataArrGlob) {
-                    insertValuesGlob(keyGlob, this.dataArrGlob[keyGlob]);
+                for (var keyGlob in this.dataArrByBody) {
+                    insertValuesGlob(keyGlob, this.dataArrByBody[keyGlob]);
                 }
                 //TODO part for localized function
-                for (var idLoc in this.dataArrLoc) {
-                     dataObj = this.dataArrLoc[idLoc];
+                for (var idLoc in this.dataArrById) {
+                     dataObj = this.dataArrById[idLoc];
                      for(var nameLoc in dataObj){
                         insertValuesLoc(idLoc, nameLoc, dataObj[nameLoc]);
                      }
                 }
             },
             printData: function(){
-                for (var k in this.dataArrGlob) {
-                    console.log( k +" --> "+ this.dataArrGlob[k]);
+                for (var k in this.dataArrByBody) {
+                    console.log( k +" --> "+ this.dataArrByBody[k]);
                 }
-                for (var j in this.dataArrLoc) {
+                for (var j in this.dataArrById) {
                     console.log("#"+j );
-                    var propObj = this.dataArrLoc[j];
+                    var propObj = this.dataArrById[j];
                     for (var g in propObj) {
                         console.log("  --> "+g+" : "+propObj[g]);
                     }
@@ -58,21 +62,50 @@ addEvent("DOMContentLoaded", document, function(event) {
             //    
             //} 
         };
-        //TODO function localized replace "insertValuesLoc"
-        function insertValuesLoc(id, name, value) {
-            var pattern = new RegExp('{{'+name+'}}', "gi");
-            var local = document.getElementById(id);
-            var locText = local.innerHTML;
-            locText = locText.replace(pattern, value);
-            local.innerHTML = locText;
-            return true;
-        }
-        function insertValuesGlob(name, value) {
-            var pattern = new RegExp('{{'+name+'}}', "gi");
-            var body = document.getElementsByTagName("body")[0];
-            var bodyText = body.innerHTML;
-            bodyText = bodyText.replace(pattern, value);
-            body.innerHTML = bodyText;
+        //function insertValuesLoc(id, name, value) {
+        //    var pattern = new RegExp('{{'+name+'}}', "gi");
+        //    var local = document.getElementById(id);
+        //    var locText = local.innerHTML;
+        //    locText = locText.replace(pattern, value);
+        //    local.innerHTML = locText;
+        //    return true;
+        //}
+        //function insertValuesGlob(name, value) {
+        //    var pattern = new RegExp('{{'+name+'}}', "gi");
+        //    var body = document.getElementsByTagName("body")[0];
+        //    var bodyText = body.innerHTML;
+        //    bodyText = bodyText.replace(pattern, value);
+        //    body.innerHTML = bodyText;
+        //    return true;
+        //}
+        //Universal function
+        function binding(elType, elName, bindName, bindValue) {
+            var elmnt, arrElmnts, elmntLikeText, pattern = new RegExp('{{'+bindName+'}}', "gi");
+            switch (elType) {
+               case 'document':
+                  elmnt = document.getElementsByTagName("html")[0];
+                  elmntLikeText = elmnt.innerHTML;
+                  elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
+                  break;
+               case 'body':
+                  elmnt = document.getElementsByTagName("body")[0];
+                  elmntLikeText = elmnt.innerHTML;
+                  elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
+                  break;
+               case 'class':
+                  arrElmnts = document.getElementsByClassName(elName);
+                  for(var k = 0; k<arrElmnts.length; k++){
+                     elmnt = arrElmnts.item(k);
+                     elmntLikeText = elmnt.innerHTML;
+                     elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
+                  }
+                  break;
+               case 'id':
+                  elmnt = document.getElementById(elName);
+                  elmntLikeText = elmnt.innerHTML;
+                  elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
+                  break;
+            }
             return true;
         }
         

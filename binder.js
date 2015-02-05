4676eae34badc8707cc699c80ca8388a9b617908
demetron <dmitriy_zh@ukr.net>
2015-02-05 15:42:20 +0200

    Global refractoring

--- binder.js
+++ binder.js
@@ -12,27 +12,48 @@ addEvent("DOMContentLoaded", document, function(event) {
     (function(){
         var binder = {
             data:{
-               byDocument:{},
-               byBody:{},
-               byId:{},
-               byClass:{},
-            }
-            addValue:function(n, v, id){
-                if (arguments.length == 3) {
-                    if(this.data.byId[id] == undefined){
-                        this.data.byId[id] = {};
-                    }
-                    this.data.byId[id][n] = v;
-                }
-                else if(arguments.length == 2) {
-                    this.data.byBody[n] = v;
-                }
+               byDocument:[],
+               byBody:[],
+               byId:[],
+               byClass:[],
             },
-            addArrValues: function(objVal){
-                for (var name in objVal) {
-                    this.addValue(name, objVal[name]);
-                }
+            //addValue:function(n, v, id){
+            //    if (arguments.length == 3) {
+            //        if(this.data.byId[id] == undefined){
+            //            this.data.byId[id] = {};
+            //        }
+            //        this.data.byId[id][n] = v;
+            //    }
+            //    else if(arguments.length == 2) {
+            //        this.data.byBody[n] = v;
+            //    }
+            //},
+            createBind: function(typeEl, nameEl, tagName, value){
+               var bindData =  {};
+               bindData.nameEl =  nameEl;  // if document or body can be empty string ''
+               bindData.tagName = tagName;
+               bindData.value = value;
+               switch (typeEl) {
+                  case 'document':
+                     this.data.byDocument.push(bindData);
+                     break;
+                  case 'body':
+                     this.data.byBody.push(bindData);
+                     break;
+                  case 'class':
+                     this.data.byClass.push(bindData);
+                     break;
+                  case 'id':
+                     this.data.byId.push(bindData);
+                     break;
+               }
             },
+            //TODO
+            //addArrValues: function(objVal){
+            //    for (var name in objVal) {
+            //        this.addValue(name, objVal[name]);
+            //    }
+            //},
             initData: function(){
                 for (var keyGlob in this.dataArrByBody) {
                     insertValuesGlob(keyGlob, this.dataArrByBody[keyGlob]);
@@ -45,16 +66,30 @@ addEvent("DOMContentLoaded", document, function(event) {
                      }
                 }
             },
-            printData: function(){
-                for (var k in this.dataArrByBody) {
-                    console.log( k +" --> "+ this.dataArrByBody[k]);
-                }
-                for (var j in this.dataArrById) {
-                    console.log("#"+j );
-                    var propObj = this.dataArrById[j];
-                    for (var g in propObj) {
-                        console.log("  --> "+g+" : "+propObj[g]);
-                    }
+            printData: function(){//type){
+                //for (var k in this.dataArrByBody) {
+                //    console.log( k +" --> "+ this.dataArrByBody[k]);
+                //}
+                //for (var j in this.dataArrById) {
+                //    console.log("#"+j );
+                //    var propObj = this.dataArrById[j];
+                //    for (var g in propObj) {
+                //        console.log("  --> "+g+" : "+propObj[g]);
+                //    }
+                //}
+                //;
+                
+                //function
+                
+                for (var k in this.data.byBody) {
+                  console.log("Body");
+                  if (this.data.byBody.length > 0) {
+                     var propObj = this.data.byBody[k];
+                     console.log(propObj.nameEl+" --> "+propObj.tagName+" - "+propObj.value);
+                  }
+                  else{
+                     console.log(" -- no binds -- ");
+                  }
                 }
             }
             //TODO
@@ -82,17 +117,17 @@ addEvent("DOMContentLoaded", document, function(event) {
         function binding(elType, elName, bindName, bindValue) {
             var elmnt, arrElmnts, elmntLikeText, pattern = new RegExp('{{'+bindName+'}}', "gi");
             switch (elType) {
-               case 'document':
+               case 'byDocument':
                   elmnt = document.getElementsByTagName("html")[0];
                   elmntLikeText = elmnt.innerHTML;
                   elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
                   break;
-               case 'body':
+               case 'byBody':
                   elmnt = document.getElementsByTagName("body")[0];
                   elmntLikeText = elmnt.innerHTML;
                   elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
                   break;
-               case 'class':
+               case 'byClass':
                   arrElmnts = document.getElementsByClassName(elName);
                   for(var k = 0; k<arrElmnts.length; k++){
                      elmnt = arrElmnts.item(k);
@@ -100,7 +135,7 @@ addEvent("DOMContentLoaded", document, function(event) {
                      elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
                   }
                   break;
-               case 'id':
+               case 'byId':
                   elmnt = document.getElementById(elName);
                   elmntLikeText = elmnt.innerHTML;
                   elmnt.innerHTML = elmntLikeText.replace(pattern, bindValue);
